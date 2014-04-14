dc7bcd9e55c49bbcb2946f72e4445db631b3dcd4
jorenca <jv_mw@abv.bg>
2014-04-14 19:53:34 +0300

    MemoryMapper class.

--- src/georgi/gaydarov/gos/compiler/translator/actionizer/MemoryMapper.java
+++ src/georgi/gaydarov/gos/compiler/translator/actionizer/MemoryMapper.java
@@ -1,20 +1,45 @@
 package georgi.gaydarov.gos.compiler.translator.actionizer;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
+/**
+ * Class responsible for mapping variable name to memory addresses.
+ * 
+ * @author Georgi Gaydarov
+ *
+ */
 public class MemoryMapper {
 	private final static int MEMORY_SIZE = 16000; // bytes
 	private Map<String, Short> variableAddress = new HashMap<String, Short>();
+	private List<Short> freedAddresses = new LinkedList<Short>();
+	private short nextFreePointer;
+	
+	/**
+	 * Constructs a memory mapper that starts mapping at a specified address.
+	 * @param startAddress - the start address.
+	 */
 	public MemoryMapper(short startAddress)
 	{
-		// TODO implement
+		nextFreePointer = startAddress;
+		// TODO implement more
 	}
 	
+	/**
+	 * Sets the memory of a variable as free and available for further allocations.
+	 * @param variableName - the variable to free.
+	 */
 	public void freeVariable(String variableName)
 	{
 		// TODO IMPLEMENT var free
 	}
+	
+	/**
+	 * Allocates memory for a variable.
+	 * @param variableName - the name of the variable for which memory should be allocated.
+	 */
 	public void allocateVariable(String variableName)
 	{
 		if(variableAddress.containsKey(variableName))
@@ -22,8 +47,29 @@ public class MemoryMapper {
 			throw new MemoryMappingException("Variable named ["+variableName+"] already declared!");
 		}
 		
-		// TODO alloc
+		if(freedAddresses.isEmpty())
+		{
+			if(nextFreePointer < MEMORY_SIZE)
+			{
+				variableAddress.put(variableName, nextFreePointer);
+				nextFreePointer++;
+			}
+			else
+			{
+				throw new MemoryMappingException("Out of memory!");
+			}
+		}
+		else
+		{
+			// TODO implement alloc from free
+		}
 	}
+	
+	/**
+	 * Returns the memory address of a variable.
+	 * @param variableName - the variable name.
+	 * @return the address where the variable is stored in memory.
+	 */
 	public short getVariableAddress(String variableName)
 	{
 		if(!variableAddress.containsKey(variableName))
